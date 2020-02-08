7beb7f81fb7193983c3f7d4aa49dbc1e69f16860
RationalGeek <jkohlhepp@gmail.com>
2020-02-08 14:10:05 -0500

    Working user setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var users = ['jktest'];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', users: users });
+});
+
+// Init router for each user
+users.forEach(function (username) {
+  require('./users/' + username)(router, username);
 });
 
 module.exports = router;
