ccaa8fe06f4e892de9b76eb969b517a81d0692de
RationalGeek <jkohlhepp@gmail.com>
2020-02-08 14:28:10 -0500

    Functioning links to user pages

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 var users = ['jktest', 'jktest2', 'jktest3'];
 
-/* GET home page. */
+// Home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', users: users });
+  res.render('index', { title: 'CSC375 / SER300 Collaborative Web App', users: users });
 });
 
 // Init router for each user
