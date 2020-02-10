b45a755ac0ec5e75ce0d18da87770382532b03a1
Eamon Duffy <eamon.duffy0@gmail.com>
2020-02-10 15:23:10 -0500

    my changes

--- routes/users/jeduffy.js
+++ routes/users/jeduffy.js
@@ -1,12 +1,18 @@
-module.exports = function(router, username) {
-  router.get('/' + username, function(req, res, next) {
+module.exports = function (router, username) {
+  router.get('/' + username, function (req, res, next) {
     res.render(
-      username + '/base', 
-      {
-         title: username + '\'s Special Page',
-         username: username
+      username + '/base', {
+        title: username + '\'s Special Page',
+        username: username
       });
   });
 
   // Set up additional routes here
+  router.get('/jeduffy/mypage', function (req, res, next) {
+    res.render(
+      username + '/mypage', {
+        title: username + '\'s personal page',
+        username: username
+      });
+  });
 };
