477d610ac684a80cac5e22d1c8b2fd8e3ee9d947
Ty <tylerdegennaro7@gmail.com>
2020-02-12 16:41:34 -0500

    added custom pages and css

--- routes/users/tddegennaro.js
+++ routes/users/tddegennaro.js
@@ -3,10 +3,27 @@ module.exports = function(router, username) {
     res.render(
       username + '/base', 
       {
-         title: username + '\'s Special Page',
+         title: username + '\'s 🏡',
          username: username
       });
   });
 
-  // Set up additional routes here
+  router.get('/' + username + '/pageone', function(req, res, next) {
+    res.render(
+      username + '/pageone', 
+      {
+         title: username + ' | 📱 ',
+         username: username
+      });
+  });
+
+  router.get('/' + username + '/pagetwo', function(req, res, next) {
+    res.render(
+      username + '/pagetwo', 
+      {
+         title: username + ' | 🎮 ',
+         username: username
+      });
+  });
+
 };
