a474fbebbe2171daa79bc284c3de42af1f564755
mcallahan9745 <mcallahan9745@gmail.com>
2020-02-14 07:43:59 -0500

    Basic webpages with Team RWBY bios. Will be adding images later but I have not figured out how to do that yet. Wanted to get something ready to be submitted.

--- routes/users/macallahan1.js
+++ routes/users/macallahan1.js
@@ -1,12 +1,44 @@
-module.exports = function(router, username) {
-  router.get('/' + username, function(req, res, next) {
+module.exports = function (router, username)
+{
+  router.get('/' + username, function (req, res, next)
+  {
     res.render(
       username + '/base', 
       {
-         title: username + '\'s Special Page',
+         title: 'Ruby Rose',
          username: username
       });
   });
 
-  // Set up additional routes here
+    // Set up additional routes here
+  router.get('/' + username + '/weissschnee', function (req, res, next)
+  {
+      res.render(
+        username + '/weissschnee',
+        {
+            title: 'Weiss Schnee',
+            username: username
+        });
+  });
+  
+  router.get('/' + username + '/blakebelladonna', function (req, res, next)
+  {
+      res.render(
+        username + '/blakebelladonna',
+        {
+            title: 'Blake Belladonna',
+            username: username
+        });
+  });
+
+  router.get('/' + username + '/yangxiaolong', function (req, res, next)
+  {
+      res.render(
+        username + '/yangxiaolong',
+        {
+            title: 'Yang Xiao Long',
+            username: username
+        });
+  });
+
 };
