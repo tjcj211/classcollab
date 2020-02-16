0db4f50d9971ecd7b29b75a85f098fdd53136b89
hdominique11 <harrison.dominique11@gmail.com>
2020-02-16 18:15:15 -0500

    hdominique page

--- routes/users/hdominique.js
+++ routes/users/hdominique.js
@@ -1,12 +1,24 @@
 module.exports = function(router, username) {
   router.get('/' + username, function(req, res, next) {
     res.render(
-      username + '/base', 
+      username + '/base',
       {
          title: username + '\'s Special Page',
          username: username
+
       });
   });
 
   // Set up additional routes here
+
+
+  router.get('/secret', function(req, res, next) {
+    res.render(
+      username + '/secret',
+      {
+         title: 'Harrison' + '\'s Secret Page',
+         username: username
+      });
+  });
+
 };
