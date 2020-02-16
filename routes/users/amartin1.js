3c9f71030d84d6ce90579baf3c6ba23ccc5999ea
Alexandra Martin <amartin1@quinnipiac.edu>
2020-02-16 15:01:09 -0500

    Baby yoda

--- routes/users/amartin1.js
+++ routes/users/amartin1.js
@@ -1,12 +1,28 @@
 module.exports = function(router, username) {
-  router.get('/' + username, function(req, res, next) {
-    res.render(
-      username + '/base', 
-      {
-         title: username + '\'s Special Page',
-         username: username
-      });
-  });
+    router.get('/' + username, function(req, res, next) {
+        res.render(
+            username + '/base',
+            {
+                title: username + '\'s Special Page',
+                username: username
+            });
+    });
 
-  // Set up additional routes here
+    router.get('/amartin1/another' , function(req, res, next){
+        res.render(
+            username + '/another',
+            {
+                title: username + '\'s Second Page',
+                username: username
+            });
+    });
+
+    router.get('/amartin1/yetanother', function(req, res, next){
+        res.render(
+            username + '/yetanother',
+            {
+                title: username + '\'s Third Page',
+                username: username
+            });
+    });
 };
