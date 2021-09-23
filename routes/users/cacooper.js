e3b524fe4b61c8f9ab89ee0f0698fb3f589e9d85
cacooper21 <58480492+cacooper21@users.noreply.github.com>
2021-09-22 23:26:01 -0400

    Second Page from cacooper

--- routes/users/cacooper.js
+++ routes/users/cacooper.js
@@ -1,12 +1,21 @@
 module.exports = function(router, username) {
   router.get('/' + username, function(req, res, next) {
     res.render(
-      username + '/base', 
+      username + '/base',
       {
          title: username + '\'s Special Page',
          username: username
       });
   });
+
+    router.get('/cacooper/another', function(req, res, next) {
+      res.render(
+        username + '/another',
+        {
+           title: username + '\'s Second Page',
+           username: username
+        });
+    });
 
   // Set up additional routes here
 };
