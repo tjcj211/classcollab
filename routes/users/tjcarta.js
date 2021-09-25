63a1a452f0b5972a657f4ba02aa924b6f1ddfc2c
tjcj211 <timcarta@hotmail.com>
2021-09-25 15:33:51 -0400

    Assignment 4.1 - Timothy Carta's Changes

--- routes/users/tjcarta.js
+++ routes/users/tjcarta.js
@@ -3,10 +3,18 @@ module.exports = function(router, username) {
     res.render(
       username + '/base', 
       {
-         title: username + '\'s Special Page',
+         title: username + '\'s Main Page',
          username: username
       });
   });
 
   // Set up additional routes here
+  router.get('/tjcarta/linkedin', function(req, res, next) {
+    res.render(
+      username + '/linkedin', 
+      {
+         title: username + '\'s LinkedIn',
+         username: username
+      });
+  });
 };
